bfc7c68a3e1445c90b96a72d22d77b35a878f8a0
thomcgn <kramer.th@outlook.de>
2026-02-11 11:27:16 +0100

    ScanPackages Explizit

--- backend/src/main/java/org/thomcgn/backend/BackendApplication.java
+++ backend/src/main/java/org/thomcgn/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package org.thomcgn.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.thomcgn.backend")
 public class BackendApplication {
 
     public static void main(String[] args) {
